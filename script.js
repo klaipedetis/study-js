a174de1475c94a13527af0a9a7aacd25e0efe7f6
klaipedetis <rshabashov@yandex.ru>
2019-07-02 00:10:16 +0100

    операции сравнения

--- script.js
+++ script.js
@@ -1,41 +1,13 @@
 
-// числа: унарные операторы
+// числа: операции сравнения
+
+console.log(3 > 2);
+console.log(3 < 2);
+console.log(5 >= 3);
+console.log(10 <= 5);
+console.log(5 === 5);
+console.log(5 != 6);
+console.log(5 == "5");
+console.log(5 != "5");
 
 
-console.log(+5);
-console.log(-5);
-
-
-let i = 5;
-
-//Префиксная запись:
-console.log(++i);
-console.log(i);
-
-// Постфиксная запись:
-
-
-console.log(i--);
-console.log(i);
-
-//Бинарные операторы
-
-console.log(3 + 4);
-console.log(3 - 4);
-console.log(2* 3);
-console.log(9 / 2);
-console.log(9 % 2);
-
-//Присваивание операции
-
-let n = 15;
-console.log("n: ", n);
-n += 3;
-//n = n + 3;
-console.log("n: ", n);
-n-=6;
-//n = n - 6; 
-console.log("n: ", n);
-n /= 3;
-//n = n / 3;
-console.log("n: ", n);
