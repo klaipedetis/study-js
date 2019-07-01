64ae9e090f12fac1b8ed7ea97b369c9857fd805f
klaipedetis <rshabashov@yandex.ru>
2019-07-01 22:52:37 +0100

    сравнение символа Symbol

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let myVar;
 
 myVar =10;
+console.log(typeof myVar);
+
 myVar = "Show money";
+console.log(typeof myVar);
 myVar = true;
+console.log(typeof myVar);
 myVar = null;
+console.log(typeof myVar);
 myVar = undefined;
+console.log(typeof myVar);
 myVar = Symbol();
+console.log(typeof myVar);
 
 myVar = {};
 
-let myArr = [];
-let regExp =/w+/g;
-let func = function(){};
-let error = Error("error message");
-let date = new Date();
 
-console.log(typeof myArr);
+let mySymbol1 = Symbol("hello");
+let mySymbol2 = Symbol("hello");
+console.log(mySymbol1 == mySymbol2);
+
