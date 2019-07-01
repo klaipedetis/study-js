c739fda8d9d686db0b3bd94dff490d055bbb87ec
klaipedetis <rshabashov@yandex.ru>
2019-07-01 22:56:49 +0100

    сравнение null

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 let myVar;
 
-myVar =10;
-console.log(typeof myVar);
-
-myVar = "Show money";
-console.log(typeof myVar);
-myVar = true;
-console.log(typeof myVar);
-myVar = null;
-console.log(typeof myVar);
-myVar = undefined;
-console.log(typeof myVar);
-myVar = Symbol();
-console.log(typeof myVar);
-
-myVar = {};
-
-
-let mySymbol1 = Symbol("hello");
-let mySymbol2 = Symbol("hello");
-console.log(mySymbol1 == mySymbol2);
+console.log(null > o);
+console.log(null >= o);
+console.log(null == o);
 
