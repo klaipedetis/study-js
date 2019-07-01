6dfec57157f5596c0eac76953865d2a3d5aa62b2
klaipedetis <rshabashov@yandex.ru>
2019-07-01 20:22:58 +0100

    add variables

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.getElementById("github").onclick = function(){
 };
 
 document.getElementById("github").addEventListener("click", message);
+
+let money = "curency";
+let income: 25;
+let addExpenses = 69;
+let deposit = 100;
+let period = 10;
+
+{
+alert("show money");
+console.log(1);
+}
