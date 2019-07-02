6491abc2807f41f59c05a58ceeeca3d62031b76d
klaipedetis <rshabashov@yandex.ru>
2019-07-02 21:21:46 +0100

    add variables income and deposit

--- str.js
+++ str.js
@@ -1,2 +1,18 @@
-let str = "Hello my friends!";
-console.log(str.length);
+//инициализация переменных
+
+let money = 600;
+    console.log(money);
+
+let income = "Доход от фриланса";
+    console.log(income);
+
+
+
+let deposit = 50;
+    if(true){
+        let deposit = 100;
+        console.log(deposit);
+    }
+  
+
+    
