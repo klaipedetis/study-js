c32ba8dba3e8c0c6bfc0db4cddafee6f08711a62
klaipedetis <rshabashov@yandex.ru>
2019-07-02 18:25:56 +0100

    математические функции

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ console.log(5 == "5");
 console.log(5 != "5");
 
 
+//математические функции
+
+console.log(Math.sqrt(25)); // Извлеение корня
+console.log(Math.pow(5, 3)); // возведение в степень
+
+console.log(Number.isInteger(5.5));//проверка числа на цело
+
+//числительность(true - целое or falce - если дробное)
