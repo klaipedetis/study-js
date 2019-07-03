f0a3a401ed84f7e94aef77e680055f4fa4d2f610
klaipedetis <rshabashov@yandex.ru>
2019-07-04 00:48:32 +0100

    подправил файл

--- homeworks3.js
+++ homeworks3.js
@@ -1,33 +1,31 @@
 
 let money = prompt("Ваш месячный доход?");
 console.log(money);
-
-let money = +prompt("Ваш месячный доход?");
 console.log(typeof money);
 
 
 let addExpenses = prompt("Перечислите возможные расходы \n за расчитывамый период через запятую");
 console.log(addExpenses)
-
-let addExpenses = prompt("Перечислите возможные расходы \n за расчитывамый период через запятую");
 console.log(typeof addExpenses);
+
 
 let deposit = prompt("“Есть ли у вас депозит в банке?”");
 console.log(Boolean (deposit));
   
 
-let mainCost = prompt("“Какие обязательные ежемесячные расходы у вас есть?” ");
+let mainCost = prompt("“Какие обязательные ежемесячные расходы у вас есть?”");
 console.log(mainCost);
 let sum = prompt("Во сколько это обойдется?");
 console.log(sum);
 
-let mainCost1 = prompt("“Какие обязательные ежемесячные расходы у вас есть?” ");
+let mainCost1 = prompt("“Какие обязательные ежемесячные расходы у вас есть?”");
 console.log(mainCost1);
 let sum1 = prompt("Во сколько это обойдется?");
 console.log(sum);
 
 
-
+let budgetMonth =  money - addExpenses + sum + sum1;
+console.log(budgetMonth);
 
 
 
