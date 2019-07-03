8006395a4645f228aeb22c8f06d9266a5963a93c
klaipedetis <rshabashov@yandex.ru>
2019-07-03 20:10:37 +0100

    added new folder

---
+++ additional tasks/new.js
@@ -0,0 +1,11 @@
+
+let money = 600;
+let budgetDay = money / 30;
+console.log(budgetDay); 
+console.log(money % budgetDay);
+
+
+let addExpenses = "налоги, машина, телефон";
+console.log(addExpenses.toLowerCase());
+addExpenses = addExpenses.split([separator] ", ");
+console.log(addExpanses);
