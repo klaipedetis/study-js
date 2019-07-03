8a0428a3d0bcbbcdb83e6e6e5a1918d62a02fd03
klaipedetis <rshabashov@yandex.ru>
2019-07-03 23:37:44 +0100

    вывел тип money

--- homeworks3.js
+++ homeworks3.js
@@ -2,3 +2,6 @@
 let money = prompt("Ваш месячный доход?");
 console.log(money);
 
+let money = prompt("Ваш месячный доход?");
+console.log(typeof money);
+
--- script.js
+++ script.js
@@ -12,10 +12,6 @@ console.log("Hello" + " " + "world");
 let str = "Hello my friends!";
 console.log(myString + str);
 
-
-let str = "Hello my friends!";
-
-
     console.log(str.length); //показывет кол-во символов в строке
     console.log(str.toUpperCase());
 
