52145d2810e3d1c7f5dce457f6e04f5036a0a42f
klaipedetis <rshabashov@yandex.ru>
2019-07-05 18:52:22 +0100

    исправлено 2е ДЗ

--- homeworks3.js
+++ homeworks3.js
@@ -1,48 +1,31 @@
-
 let money = prompt("Ваш месячный доход?");
-console.log(money);
-
+let income = promt("вёрстка, такси, фотография");
+let deposit = confirm("“Есть ли у вас депозит в банке?”");
 let mainCost = prompt("“Какие обязательные ежемесячные расходы у вас есть?”");
-console.log(mainCost);
-let sum = prompt("Во сколько это обойдется?");
-console.log(sum);
-
-let mainCost1 = prompt("“Какие обязательные ежемесячные расходы у вас есть?”");
-console.log(mainCost1);
-let sum1 = prompt("Во сколько это обойдется?");
-console.log(sum);
-
-let budgetMonth =  money - sum - sum1;
-console.log(budgetMonth);
-let budgetDay = budgetMonth / 30;
-console.log(budgetDay);
-
-/*Расчитываем за сколько месяцев будет достигнута миссия:
-mision / budgetMonth*/
-
-let mission = 7100000;
-console.log(Math(mission / budgetMonth));
-
-
-
-let deposit = prompt("“Есть ли у вас депозит в банке?”");
-console.log(Boolean (deposit));
- 
 let addExpenses = prompt("Перечислите возможные расходы \n за расчитывамый период через запятую");
-console.log(addExpenses)
-console.log(typeof addExpenses);
 
-let money = +prompt("Ваш месячный доход?");
+console.log(sum);
+console.log(sum1);
+console.log(mainCost);
+console.log(deposit);
+
+console.log(typeof mainCost);
 console.log(typeof money);
 
-let mainCost1 = prompt("“Какие обязательные ежемесячные расходы у вас есть?”");
-console.log(typeof mainCost1);
+console.log(Math(mission / budgetMonth));
+console.log(addExpenses.split ", ");
+console.log(typeof addExpenses),
 
 
-let budgetDay = budgetMonth / 30;
-console.log(Math.floor(budgetDay));
+    if (budgetDay >= 800);
+console.log(("Высокий уровень дохода")); {
+    
+} else if ((budgetDay >= 300) && (budgetDay <= 800));
+console.log(confirm("Средний уровень дохода")); {
 
-if (budgetDay >= 800) console.log(confirm("Высокий уровень дохода"));
-if budgetDay 300 >= 800 console.log(confirm("Средний уровень дохода"));
-if (budgetDay <= 300) console.log(confirm("Низкий уровень дохода"));
-if (budgetDay <= 0) console.log(confirm("Что то пошло не так"));
+} else if if (budgetDay <= 300);
+console.log(("Низкий уровень дохода"));
+}
+else if (budgetDay <= 0);
+console.log(("Что то пошло не так"));
+}
