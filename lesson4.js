1b586a49f73a6a12b68475fd85506b98ed327ea1
klaipedetis <rshabashov@yandex.ru>
2019-07-11 20:04:56 +0100

    Выполненое 4е задание

--- lesson4.js
+++ lesson4.js
@@ -1,15 +1,15 @@
 let money = prompt("Ваш месячный доход?"),
     income = alert("вёрстка, такси, фотография"),
-    deposit = confirm("“Есть ли у вас депозит в банке?”"),
-    mainCost = prompt("“Какие обязательные ежемесячные расходы у вас есть?”"),
-    sum = prompt("Во сколько это Вам обойдётся?"),
-   
-    sum1 = prompt("Во сколько это Вам обойдётся?"),
+    deposit = confirm("Есть ли у вас депозит в банке?"),
+    mainCost = prompt("Какие обязательные ежемесячные расходы у вас есть?"),
+    sum = +prompt("Во сколько это Вам обойдётся?"),
+    mainCost1 = prompt("“Какие обязательные ежемесячные расходы у вас есть?”"),
+    sum1 = +prompt("Во сколько это Вам обойдётся?"),
     addExpenses = prompt("Перечислите возможные расходы \n за расчитывамый период через запятую");
-    mission = 710000;
+mission = 710000;
 
 console.log(addExpenses.split(", "));
-console.log(typeof (money), typeof (income), typeof (deposit));
+
 
 let showTypeof = function (item) {
     console.log(item, typeof item);
@@ -21,40 +21,41 @@ showTypeof(deposit);
 
 
 
-let budgetMounth = money - sum - sum1;
-console.log(budgetMounth);
-console.log(Math.ceil(mission / budgetMounth));
+let budgetMonth = money - sum - sum1;
 
-let budgetDay = budgetMounth / 30;
-console.log(Math.floor(budgetDay));
 
-function getStatusIncome(){
-    if (budgetDay < 300){
-        return("Низкий уровень дохода");
-    } else if (budgetDay <=800) {
-        return("Средний уровень дохода");
+let budgetDay = budgetMonth / 30;
+
+
+function getStatusIncome() {
+    if (budgetDay < 300) {
+        return ("Низкий уровень дохода");
+    } else if (budgetDay <= 800) {
+        return ("Средний уровень дохода");
     } else {
-        return("Высокий уровень дохода");
+        return ("Высокий уровень дохода");
     }
 }
 console.log("getStatusIncome(): ", getStatusIncome());
 
 
-       let getExpensesMonth = function getExpensesMonth() {
-            console.log(sum + sum1);
-        }
-                console.log(getExpensesMonth());
+let getExpensesMonth = function getExpensesMonth() {
+    return sum + sum1;
+}
+console.log(getExpensesMonth());
 
-            let getAcumulatedMonth;
 
-        let getAccumulatedMonth = function getAccumulatedMonth() {
-                console.log(getBudgetMonth - getExpensesMonth);
-        }
 
-       getAcumulatedMonth();
-        
-        function getTargetMonth(){
-            console.log(Math.floor(mission / getAccumulatedMonth));
-        };
-            getTargetMonth();
+
+let getAccumulatedMonth = function getAccumulatedMonth() {
+    return money - getExpensesMonth();
+}
+console.log(getAccumulatedMonth());
+
+
+function getTargetMonth() {
+    
+    console.log(Math.floor(mission / getAccumulatedMonth()));
+};
+getTargetMonth();
 
